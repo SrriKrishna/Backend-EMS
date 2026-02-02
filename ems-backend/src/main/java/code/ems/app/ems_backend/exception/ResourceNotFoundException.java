a6208b7f577f631f9787cd3187baf67bd1e567e0
SrriKrishna N <138118593+SrriKrishna@users.noreply.github.com>
2026-02-02 17:11:30 +0530

    Backend Committed

---
+++ ems-backend/src/main/java/code/ems/app/ems_backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package code.ems.app.ems_backend.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message){
+
+        super(message);
+    }
+}
